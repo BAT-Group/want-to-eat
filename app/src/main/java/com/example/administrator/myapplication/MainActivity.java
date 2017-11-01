26515b069150a521874718dbc0493200e2074fc5
ZhangPeng <zhangpeng@ilextech.com>
2017-11-01 17:56:00 +0800

    add kotlin、log jar包

--- app/src/main/java/com/example/administrator/myapplication/MainActivity.java
+++ app/src/main/java/com/example/administrator/myapplication/MainActivity.java
@@ -4,11 +4,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
 
+import com.orhanobut.logger.AndroidLogAdapter;
+import com.orhanobut.logger.Logger;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Logger.addLogAdapter(new AndroidLogAdapter());
         setContentView(R.layout.activity_main);
         Toast.makeText(getApplicationContext(),"toast",Toast.LENGTH_LONG);
     }
