b56af7f1e689f1ed34eab9c0c658f58d62dc1b79
ZhangPeng <zhangpeng@ilextech.com>
2017-11-08 11:20:26 +0800

    add 标注jar包

--- app/src/main/java/com/example/administrator/application/MainActivity.java
+++ app/src/main/java/com/example/administrator/application/MainActivity.java
@@ -7,13 +7,19 @@ import android.widget.Toast;
 import com.orhanobut.logger.AndroidLogAdapter;
 import com.orhanobut.logger.Logger;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
+//    @BindView
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Logger.addLogAdapter(new AndroidLogAdapter());
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
         Toast.makeText(getApplicationContext(),"toast",Toast.LENGTH_LONG);
     }
 }
